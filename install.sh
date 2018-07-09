72e2b62720e836500c38bdd80ec7b65463154b6b
davidalbertonogueira <david_alberto_nogueira@hotmail.com>
2018-07-09 20:01:29 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 apt-get update
 apt-get install -y wget build-essential automake autoconf git unzip libconfig++-dev libicu-dev python3 cython3 python3-flask
 
+apt-get install -y python3-pip
+pip3 install --upgrade pip
+pip3 install flask
+pip3 install flask_cors
+pip3 install wtforms
+pip3 install requests
+pip3 install tornado
+pip3 install gevent
+pip3 install sortedcontainers
+
 current_path="$(pwd)"
 outer_context="../.."
 
