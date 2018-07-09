5ed63fbbeedbcfc668c0027bb85f01e481784b20
davidalbertonogueira <david_alberto_nogueira@hotmail.com>
2018-07-09 17:30:06 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+apt-get update
+apt-get install -y wget build-essential automake autoconf git unzip libconfig++-dev libicu-dev python3 cython3 python3-flask
+
+cd TurboParser
+./install_deps.sh
+rm missing
+aclocal
+autoconf -f
+automake --add-missing
+./configure && make
+cd libturboparser
+make
+cd ../..
+
+cd TurboTextAnalysis/TurboTextAnalysis
+current_path="$(pwd)"
+find="/pba/workspace"
+sed -ie "s?${find}?${current_path}?g" Makefile
+make
+cd ..
+cd TurboTextAnalysisPython
+sed -ie "s?${find}?${current_path}?g" setup.py
+python3 setup.py build_ext --inplace
+
+add_to_ld_library_path=":/pba/workspace/TurboParser/deps/local/lib/:/pba/workspace/TurboParser/libturboparser/:/pba/workspace/TurboTextAnalysis/TurboTextAnalysis:/pba/workspace/TurboTextAnalysis/TurboTextAnalysisPython"
+add_to_ld_library_path=${add_to_ld_library_path//$find/$current_path}
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${add_to_ld_library_path}
+
+
+
