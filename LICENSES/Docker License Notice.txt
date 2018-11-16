0c8735eb79ea668f7e64dbf760d859fff1ce53ab
davidalbertonogueira <david_alberto_nogueira@hotmail.com>
2018-11-16 12:30:27 +0000

    Update licensing

---
+++ LICENSES/Docker License Notice.txt
@@ -0,0 +1,7 @@
+Priberam’s TurboParser Entity Tagging is an open-source version of SUMMA’s Entity Tagging module, made on top of TurboParser.
+
+Copyright 2018 by PRIBERAM INFORMÁTICA, S.A. - www.priberam.com
+
+You have access to this product in the scope of Project "SUMMA - Project Scalable Understanding of Multilingual Media", Project Number 688139, H2020-ICT-2015.
+
+Usage subject to The terms & Conditions of the "Priberam TurboParser Entity Tagging OS Software License" available at https://www.priberam.pt/docs/Priberam_TurboParser_Entity_Tagging_OS_Software_License.pdf
--- TurboTextAnalysisServer.py
+++ TurboTextAnalysisServer.py
@@ -1,3 +1,8 @@
+#This module is part of “Priberam’s TurboParser Entity Tagging”, an open-source version of SUMMA’s Entity Tagging module, made on top of TurboParser.
+#Copyright 2018 by PRIBERAM INFORMÁTICA, S.A. - www.priberam.com
+#You have access to this product in the scope of Project "SUMMA - Project Scalable Understanding of Multilingual Media", Project Number 688139, H2020-ICT-2015.
+#Usage subject to The terms & Conditions of the "Priberam TurboParser Entity Tagging OS Software License" available at https://www.priberam.pt/docs/Priberam_TurboParser_Entity_Tagging_OS_Software_License.pdf
+
 from turboparser import PyCPBSSink, PyCppToPyTurboSink, PyCTurboTextAnalysis, PyLoadOptions, PyAnalyseOptions
 import os
 
